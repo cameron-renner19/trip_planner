f903f4b6788efab61208fc5ab20c6abdb5680581
Cameron <cameron@localhost.localdomain>
2025-01-13 18:48:48 -0500

    some beginning files

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Main file that will start our program
+Any initialization will be run here (db, flat files, etc.)
+GUI will be kicked off here
+"""
+
+import os
+import pandas as pd
+from dataclasses import dataclass
+
+def main():
+    # code here
+    print("Running the Program")
+
+if __name__ == "__main__":
+    main()
